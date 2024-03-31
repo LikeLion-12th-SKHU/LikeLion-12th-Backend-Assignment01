7ac7e87268b207edca67b95f93f71cde77cf5814
soohyun <sss4920@naver.com>
2024-03-31 18:47:10 +0900

    refactor: pdf만 보고 했다가 빼먹은 케이스 수정

--- first-java-assignment/src/assign2/Test1.java
+++ first-java-assignment/src/assign2/Test1.java
@@ -37,23 +37,31 @@ class Cylinder{
 		this.y += dy;
 		return this;
 	}
-	public void printCircle(){
-		System.out.println("x: "+this.x +"\ny: "+ this.y);
+	void print() {
+		System.out.println("<"+this.x+","+this.y+":"+this.r+">");
 	}
 
 }
-
+// 메인메서드 수행시 이렇게 출력되도록
+//<13,15:6.0>
+//113.04
+//1130.4
+//602.88
+//<17,25:10.0>
+//314.0
+//1570.0
+//942.0
 public class Test1 {
 	public static void main(String[] args) {
 			Cylinder c1 = new Cylinder(3,5, 6);
 
-			c1.move(10,10);
+			c1.move(10,10).print();
 			System.out.println(c1.circleArea());
 			System.out.println(c1.volume());
 			System.out.println(c1.surfaceArea());
 
 			Cylinder c2 = new Cylinder(5,8, 10, 5);
-			c2.move(12,17).printCircle();
+			c2.move(12,17).print();
 			System.out.println(c2.circleArea());
 			System.out.println(c2.volume());
 			System.out.println((float) c2.surfaceArea());
