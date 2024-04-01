b0ccbf8ef0b52f6c47d9db0febf951d80b163479
Kang Duoh <kduoh99@gmail.com>
2024-04-01 10:01:16 +0900

    refactor: 접근제어자, 메서드명 수정 및 this 키워드 추가

--- kangduoh/first-java-assignment/src/assigns/Test1.java
+++ kangduoh/first-java-assignment/src/assigns/Test1.java
@@ -3,13 +3,13 @@ package assigns;
 // 다음 조건을 만족하도록 Cylinder 클래스를 작성합니다.
 class Cylinder {
     // 원주율 3.14를 정적 상수 PI로 선언과 동시에 초기화
-    static final double PI = 3.14;
+    private static final double PI = 3.14;
     // 정수형 원의 중심 좌표 x, y를 선언
-    int x, y;
+    private int x, y;
     // 실수형 원의 반지름 r을 선언
-    double r;
+    private double r;
     // 정수형 원기둥의 높이 height를 10으로 선언과 동시에 초기화
-    int height = 10;
+    private int height = 10;
 
     // 생성자1: 정수 인자 x, y 와 실수 인자 r을 전달 받아서 해당 필드 값을 초기화
     Cylinder(int x, int y, double r) {
@@ -27,13 +27,13 @@ class Cylinder {
     }
 
     // circleArea 메서드: PI를 이용하여 원의 면적 반환
-    public double area() {
+    public double circleArea() {
         return PI * Math.pow(r, 2);
     }
 
     // volume 메서드: 면적과 높이를 이용하여 부피를 반환
     public double volume() {
-        return area() * height;
+        return this.circleArea() * height;
     }
 
     // surfaceArea 메서드: PI를 이용하여 원기둥의 겉넓이를 반환
@@ -69,13 +69,13 @@ public class Test1 {
     public static void main(String[] args) {
         Cylinder c1 = new Cylinder(3,5, 6);
         c1.move(10,10).print();
-        System.out.println(c1.area());
+        System.out.println(c1.circleArea());
         System.out.println(c1.volume());
         System.out.println(c1.surfaceArea());
 
         Cylinder c2 = new Cylinder(5,8, 10, 5);
         c2.move(12,17).print();
-        System.out.println(c2.area());
+        System.out.println(c2.circleArea());
         System.out.println(c2.volume());
         System.out.println((float) c2.surfaceArea());
     }
