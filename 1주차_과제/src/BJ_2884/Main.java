773a746870a57aebde74c948095ca05b8d0fa8bb
AWESOMEGUY5 <50280161+AWESOMEGUY5@users.noreply.github.com>
2024-04-01 12:52:04 +0900

    1차 수정
    시간은 음수개념이 없다는 것을 까먹고 있었습니다; 
    0이거나 0이 아니거나(양수)로 조건문을 수정했습니다.

--- 1주차_과제/src/BJ_2884/Main.java
+++ 1주차_과제/src/BJ_2884/Main.java
@@ -11,12 +11,12 @@ public class Main
         int M = scanner1.nextInt();
 
         M = M - 45;
-        if (H <= 0 && M < 0)
+        if (H == 0 && M < 0)
         {
             H = 23;
             M = 60 + M;
         }
-        else if (H > 0 && M < 0)
+        else if (H != 0 && M < 0)
         {
             H--;
             M = 60 + M;
